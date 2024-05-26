96451e14ddae48abea4d25ff4525fba2370f94b9
Ayocodes24 <153740193+Ayocodes24@users.noreply.github.com>
2024-05-26 20:25:38 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,120 +1,133 @@
-<?php
-  include "floating-login-signup/partials/login.php";
-?>
-
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>How to Design Login & Registration Form Transition</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="style.css">
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Online Food Ordering</title>
+ 	
+
+<?php
+  include('./../floating-login-signup/partials/_dbconnect.php');
+	session_start();
+  if(!isset($_SESSION['login_id'])){
+    header('location:../floating-login-signup/index.php');
+  }
     
-    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700,800&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    
-    <div class="bacgroundimg">
-      <img src="" alt="">
+  include('./header.php'); 
+ // include('./auth.php'); 
+ ?>
+
+</head>
+<style>
+	body{
+    background: #80808045;
+  }
+</style>
+
+<body>
+	<?php include 'navbar.php' ?>
+  <div class="toast" id="alert_toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-body text-white">
     </div>
-    
-    <div class="cont">
-      <form action="floating-login-signup/partials/login.php" method = "post">
-        <div class="form sign-in">
-          <h2>Sign In</h2>
-          <label>
-            <span>Email</span>
-            <input type="email" name = "lemail" required>
-          </label>
-          <label>
-            <span>Password</span>
-            <input type="password" name = "lpassword" required>
-          </label>
-          <div class="warning">
+  </div>
+  <main id="view-panel" >
+      <?php $page = isset($_GET['page']) ? $_GET['page'] :'home'; ?>
+  	<?php include $page.'.php' ?>
+  	
 
-          </div>
-          <input type="submit" class="submit" value="Login" name="sign_in" >
-          <!-- <p class="forgot-pass">Forgot Password ?</p> -->
-    
-          <div class="social-media">
-            <ul>
-              <li><img src="images/facebook.png"></li>
-              <li><img src="images/twitter.png"></li>
-              <li><img src="images/linkedin.png"></li>
-              <li><img src="images/instagram.png"></li>
-            </ul>
-          </div>
-        </div>
-      </form>
-      <form action="floating-login-signup/partials/signUp.php" method = "post">
-        <div class="sub-cont">
-          <div class="img">
-            <div class="img-text m-up">
-              <h2>New here?</h2>
-              <p>Sign up and discover great amount of new opportunities!</p>
-            </div>
-            <div class="img-text m-in">
-              <h2>One of us?</h2>
-              <p>If you already has an account, just sign in. We've missed you!</p>
-            </div>
-            <div class="img-btn">
-              <span class="m-up">Sign Up</span>
-              <span class="m-in">Sign In</span>
-            </div>
-          </div>
-          <div class="form sign-up">
-            <h2>Sign Up</h2>
-            <label>
-              <span>Name</span>
-              <input type="text" name = "name" required>
-            </label>
-            <label>
-              <span>Email</span>
-              <input type="email" name = "email" required>
-            </label>
-            <label>
-              <span>Phone Number</span>
-              <input type="text" id="phone" name="phone" pattern="[0-9]{10}" maxlength="10" required oninvalid="this.setCustomValidity('Phone number must contains exact 10 digits for 0-9');" oninput="this.setCustomValidity('');">
-            </label>
-            <label>
-              <span>Password</span>
-              <input type="password" name="password" id="password" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$" required oninvalid="this.setCustomValidity('Password must be at least 6 characters long and contain at least one lowercase letter, one uppercase letter, and one digit.');" oninput="this.setCustomValidity('');">
-            </label>
-            <!-- <p id="password-error" style="color: red;"></p> -->
-            <label> 
-              <span>Confirm Password</span>
-              <input type="password" name = "cpassword" required>
-            </label>
-            <!-- <button type="button" class="submit" name = "sign_up">Sign Up Now</button> -->
-            <input type="submit" class="submit" value="Sign Up Now" name="sign_up" >
-          </div>
-        </div>
-      </form>
+  </main>
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+<div class="modal fade" id="confirm_modal" role='dialog'>
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title">Confirmation</h5>
+      </div>
+      <div class="modal-body">
+        <div id="delete_content"></div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" style = "width:100px"class="btn btn-primary" id='confirm' onclick="">Continue</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+      </div>
     </div>
-    
-  <script type="text/javascript" src="script.js"></script>
+  </div>
+  <div class="modal fade" id="uni_modal" role='dialog'>
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title"></h5>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+      </div>
+      </div>
+    </div>
+  </div>
+</body>
+<script>
+	 window.start_load = function(){
+    $('body').prepend('<di id="preloader2"></di>')
+  }
+  window.end_load = function(){
+    $('#preloader2').fadeOut('fast', function() {
+        $(this).remove();
+      })
+  }
 
-  <script>
-    // const passwordInput = document.getElementById('password');
-    // const passwordError = document.getElementById('password-error');
+  window.uni_modal = function($title = '' , $url=''){
+    start_load()
+    $.ajax({
+        url:$url,
+        error:err=>{
+            console.log()
+            alert("An error occured")
+        },
+        success:function(resp){
+            if(resp){
+                $('#uni_modal .modal-title').html($title)
+                $('#uni_modal .modal-body').html(resp)
+                $('#uni_modal').modal('show')
+                end_load()
+            }
+        }
+    })
+}
+window._conf = function($msg='',$func='',$params = []){
+     $('#confirm_modal #confirm').attr('onclick',$func+"("+$params.join(',')+")")
+     $('#confirm_modal .modal-body').html($msg)
+     $('#confirm_modal').modal('show')
+  }
+   window.alert_toast= function($msg = 'TEST',$bg = 'success'){
+      $('#alert_toast').removeClass('bg-success')
+      $('#alert_toast').removeClass('bg-danger')
+      $('#alert_toast').removeClass('bg-info')
+      $('#alert_toast').removeClass('bg-warning')
 
-    // passwordInput.addEventListener('input', validatePassword);
-
-    // function validatePassword() {
-    //   const password = passwordInput.value;
-    //   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
-
-    //   if (!passwordRegex.test(password)) {
-    //     passwordError.textContent = 'Password must be at least 6 characters long and one lowercase letter, one uppercase letter,one digit.';
-    //   } else {
-    //     passwordError.textContent = '';
-    //   }
-    // }
-
-    // function handleInvalidPassword() {
-    // passwordInput.setCustomValidity('Please enter a password.');
-    // }
-  </script>
-
-  </body>
+    if($bg == 'success')
+      $('#alert_toast').addClass('bg-success')
+    if($bg == 'danger')
+      $('#alert_toast').addClass('bg-danger')
+    if($bg == 'info')
+      $('#alert_toast').addClass('bg-info')
+    if($bg == 'warning')
+      $('#alert_toast').addClass('bg-warning')
+    $('#alert_toast .toast-body').html($msg)
+    $('#alert_toast').toast({delay:3000}).toast('show');
+  }
+  $(document).ready(function(){
+    $('#preloader').fadeOut('fast', function() {
+        $(this).remove();
+      })
+  })
+</script>	
 </html>
